3593c2b64d4a845920207310ead70cb70cb4628a
Mayank <ms8955355@gmail.com>
2025-09-16 22:37:59 +0900

    Apart 1st

---
+++ src/components/NavigationPage/NavigationPage.tsx
@@ -0,0 +1,20 @@
+const NavigationPage = () =>{
+    return(
+        <>
+            <div>
+                Customer
+            </div>
+            <div>
+                Store
+            </div>
+            <div>
+                HomePage
+            </div>
+            <div>
+                <image />
+            </div>
+        </>
+    );
+};
+
+export default NavigationPage;
